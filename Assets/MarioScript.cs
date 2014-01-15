ec795b17ded2b93ace5d186979ba45dd5bb7d603
rkreiter <ryankreiter@yahoo.com>
2014-01-15 13:19:16 -0500

    Started adding mario

---
+++ Assets/MarioScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarioScript : MonoBehaviour {
+
+	public GameObject 	marioPreFab;
+	public float 		speed = 0.001f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxis("Horizontal");
+		Vector2 pos = transform.position;
+		pos.x += h*speed;
+		
+		if (Input.GetKey (KeyCode.X)) {
+			pos.y += speed;
+		}
+		transform.position = pos;
+	}
+}
