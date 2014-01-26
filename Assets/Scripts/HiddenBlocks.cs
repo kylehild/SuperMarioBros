adb2d0faccf54b5bdc51fa134d92ee981aeb6439
kylehild <kylehild@umich.edu>
2014-01-26 17:14:59 -0500

    Started doing tuff with enemies and figuring out what Ryan did

---
+++ Assets/Scripts/HiddenBlocks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiddenBlocks : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
