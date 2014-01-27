4bfae7c4cc55342fdb9b6e0624fe8efefdbed427
rkreiter <ryankreiter@yahoo.com>
2014-01-27 04:47:18 -0500

    Crapy version of collision detection implemented.
    Also enemy death animation and destruction implemented.

--- Assets/Scripts/GoombaController.cs
+++ Assets/Scripts/GoombaController.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class GoombaController : MonoBehaviour {
 
-	public float 		speed = 10f;
+	public float 		speed = -4f;
+	public float 		flipping = 0f;
+	public float 		squishTimer = 15f;
 	public bool 		facingRight = true;
+	public bool 		squished = false;
 	private	GameObject	rightBoundary;
 	private Animator	anim;
 
@@ -25,6 +28,13 @@ public class GoombaController : MonoBehaviour {
 			vel.x = speed;
 			rigidbody2D.velocity = vel;
 		}
+
+		if(flipping != 0) flipping--;
+
+		if(squished){
+			if(squishTimer > 0) squishTimer--;
+			else DestroyObject(gameObject);
+		}
 	}
 
 	void Flip(){
@@ -32,29 +42,35 @@ public class GoombaController : MonoBehaviour {
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
-		Vector3 theVelocity = rigidbody2D.velocity;
-		theVelocity.x *= -1;
-		rigidbody2D.velocity = theVelocity;
+		speed *= -1;
+		flipping = 2f;
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		
-		Vector2 pos = transform.position;
-		RaycastHit2D top = Physics2D.Raycast(new Vector2(pos.x, pos.y+1.0001f), Vector2.up, 1f);
-		RaycastHit2D bottom = Physics2D.Raycast(new Vector2(pos.x, pos.y-0.0001f), -Vector2.up, 1f);
-		RaycastHit2D left = Physics2D.Raycast(new Vector2(pos.x-0.5001f, pos.y+0.5f), Vector2.right, 1f);
-		RaycastHit2D right = Physics2D.Raycast(new Vector2(pos.x+0.5001f, pos.y+0.5f), -Vector2.right, 1f);
-		
-		if(top != null)
-		{
-			Debug.Log("Top");
-		}
-		if(bottom != null)
-		{
 
+		Vector2 pos = transform.position;
+		RaycastHit2D left =  Physics2D.Raycast(new Vector2(pos.x-0.5f, pos.y+0.5f), -Vector2.right, 0.1f);
+		RaycastHit2D right =  Physics2D.Raycast(new Vector2(pos.x+0.5f, pos.y+0.5f), Vector2.right, 0.1f);
+		RaycastHit2D up =  Physics2D.Raycast(new Vector2(pos.x, pos.y+1f), Vector2.up, 0.1f);
+		RaycastHit2D down =  Physics2D.Raycast(pos, -Vector2.up, 0.1f);
+
+		if(up.transform != null){
+			//Debug.Log("hit on top");
+			anim.SetTrigger("Squished");
+			squished = true;
 		}
-		if(right != null || left != null){
-			Flip ();
+		if(down.transform != null){
+			//Debug.Log("hit on bottom");
+		}
+		if(left.transform != null){
+			//Debug.Log("hit on left");
+			if(left.collider.gameObject.name != "Mario") Flip ();
+			else DestroyObject(left.collider.gameObject);
+		}
+		if(right.transform != null){
+			//Debug.Log("hit on right");
+			if(right.collider.gameObject.name != "Mario") Flip ();
+			else DestroyObject(right.collider.gameObject);
 		}
 	}
 }
